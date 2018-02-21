d8432e997fe26f3f0a67dcdb6cc314209a5e865c
Zafar Khan <ameen@Admins-MacBook-Pro-2.local>
2018-02-21 16:47:27 +0530

    toggle slide-panel overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  openOverlay:boolean = false;
+
+  constructor(private router: Router) {
+    //Track route change event
+    this.router.events.subscribe(event => {
+      if(event.constructor.name === "NavigationStart") {
+        this.openOverlay = false;
+      }
+    });
+  }
+
+  //side panel show/hide
+  openNav()
+  {
+    this.openOverlay = true;
+  }
+  closeNav()
+  {
+    this.openOverlay = false;
+  }
+  getOverlayWidth() {
+    if(this.openOverlay) {
+      return "100%";
+    } else {
+      return "0%";
+    }
+  }
+  //End side panel show/hide
 }
